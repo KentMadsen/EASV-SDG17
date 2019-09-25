fbb5033cfda7df663cb54e0efbb84d4d730a2458
Kent vejrup Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-09-25 11:52:29 +0200

    current workspace

--- footer.php
+++ footer.php
@@ -3,3 +3,15 @@
     echo "Footer Area"
     ?>
 </footer>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136109379-3">
+</script>
+    
+<script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-136109379-3');
+</script>
